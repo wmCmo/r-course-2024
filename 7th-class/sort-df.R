83669170c6a92bee28d2e5275f958d1bb74f88e7
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2024-04-30 14:06:01 +0900

    Seventh class
    We learned about the data frame and how to manipulate them

---
+++ 7th-class/sort-df.R
@@ -0,0 +1,10 @@
+df = data.frame(
+  names = c('Alice', 'Bob', 'Chalie', 'Dave', 'Emma', 'Felix', 'Grek', 'Harry'),
+  height = c(189, 183, 181, 174, 182, 170, 163, 190)
+)
+
+sort_df = function(df, is_ascending) {
+  print(df[order(df$height, decreasing = is_ascending), ])
+}
+
+sort_df(df, FALSE)
